c3ab059e3943dc4ba42d73ccbab51a6b46f66a5a
krishnamcm <maisuriakrishnam@gmail.com>
2025-03-19 16:23:20 -0500

    Update 4_CoinCalculator.py

--- 4_CoinCalculator.py
+++ 4_CoinCalculator.py
@@ -1,4 +1,4 @@
-def dpsolve(N,K,coins):
+def dpsolve(n,k,coins):
     INF=float('inf')
     dp=[INF]*(K+1) #initialize dp list
     dp[0]=0 #this is the base case of 0 coins needed to make 0
@@ -9,6 +9,6 @@ def dpsolve(N,K,coins):
                 dp[i]=min(dp[i],dp[i-coin]+1) #get the smaller value for optimal solution
     return dp[K] if dp[K]!=INF else -1  #if K can't be formed then return -1
 
-N,K=map(int,input().split())
+n,k=map(int,input().split())
 coins=list(map(int,input().split()))
-print(dpsolve(N,K,coins))
+print(dpsolve(n,k,coins))
