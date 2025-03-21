f3fd2014e5ba9b86497602f27a91418c1a5104d0
krishnamcm <maisuriakrishnam@gmail.com>
2025-03-20 21:14:26 -0500

    Update 5_Autocorrect.py

--- 5_Autocorrect.py
+++ 5_Autocorrect.py
@@ -7,24 +7,22 @@ for i in range(d): #creates dictionary of correct words
 for j in range(w):
     for keys in correctwords: #sets default min hemming distance
         correctwords[keys]=100
-        min=101
+        minimum=101
     wrongword=input() #takes in incorrect words
     wrongword=list(wrongword)
     for keys in correctwords:
         theword=list(keys) #gets each correct word from dictionary to compare to wrong word
-        if(len(theword)!=len(wrongword)): #if lengths are different, no point in checking
-            continue
         hemming=0
-        for k in range(len(wrongword)): #loops through each character and checks number of characters that are different
+        hemming+=abs(len(theword)-len(wrongword)) #accounts for additional letters
+        for k in range(min(len(theword),len(wrongword))): #loops through each character and checks number of characters that are different
             if(wrongword[k]!=theword[k]):
                 hemming+=1 #count for number of substitutions
         correctwords[keys]=hemming #gets hemming distance between each correct word and wrong word
     ans=""
     for keys in correctwords: #finding smallest hemming distance
-        if correctwords[keys]<min:
-            min=correctwords[keys] #update each time there is smaller hemming distance
+        if correctwords[keys]<minimum:
+            minimum=correctwords[keys] #update each time there is smaller hemming distance
             ans=keys #records word
     anslist.append(ans)
-
 for items in anslist:
     print(items)
