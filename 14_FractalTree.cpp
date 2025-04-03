a851a9600b38ef36c6e839dc1f37340df22ebeed
Vineet Vadrevu <vineetvadrevu@gmail.com>
2025-04-03 02:40:40 -0500

    Update 14_FractalTree.cpp

--- 14_FractalTree.cpp
+++ 14_FractalTree.cpp
@@ -6,7 +6,7 @@ using ll=long long;
 //notice that 2+(2^i) will always be even, thus we can tell number of healthy branches through casework
 //even - odd = odd > and even - even is even, so we only need to find the parity of infected branches 
 
-int bruteForceGrowth(int n);
+int bruteForceGrowth(int n); //scroll down to see a brute force implementation
 //every infected branch infects floor(i^log10(i)+ abs(3cos(i))) for every already infected branch plus an extra i branches (where i is the time)
 
 int growth(int i)
@@ -19,14 +19,13 @@ void solve()
 {
     int n; 
     cin>>n;
-    int x=1,y=0,z=0; //x is initial, y is the  fractal growth and z is the extra 
+    int x=1,y=0,z=0; //x is initial, y is the  fractal growth and z is the extra i branches grown at time i
     //1 represents odd and 0 represents even -> this keeps the memory of the variables small 
 
     for(int i=0; i<=n; i++)
     {
         y=growth(i);
         z=i%2;
-        
         //notice that: new branches = (old_branches +(old_branches*growth(i)))+i -> when will this be odd or even? x+(xy)+z = x(1+y)+z
         if(x^(x&y)^z==1){x=1;} //use bitwise operations to determine when the expression is even or odd, another approach is simple casework
         else{x=0;}   
@@ -38,31 +37,23 @@ void solve()
 
 int main()
 {
-    freopen("input1.in","r",stdin);
-    freopen("output1.out","w",stdout);
     int t;
     cin>>t;
     while(t--)
     {
         solve();
     }
-    
-    return 0;
 }
 
 
 
 int bruteForceGrowth(int n)
 { //this is the brute force calculation of the number of branches at t = n
-    
     ll branches=1;
-    int x=1;
     for(int i=1; i<=n; i++)
     {
-        
         branches=branches+(branches*(growth(i)))+i;
         //remove the mod 2 in the growth function to get the actual number of branches
-
     }
     return branches;
 }
