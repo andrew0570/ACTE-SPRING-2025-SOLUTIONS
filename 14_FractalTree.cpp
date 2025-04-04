e66f0cc7b28caa545bc7b330bdfc642645ad0074
Vineet Vadrevu <vineetvadrevu@gmail.com>
2025-04-04 00:27:26 -0500

    Update 14_FractalTree.cpp

--- 14_FractalTree.cpp
+++ 14_FractalTree.cpp
@@ -2,26 +2,21 @@
 using namespace std;
 using ll=long long;
 #define dbg(x) cout << #x << " = " << (x) << endl;
-
 //notice that 2+(2^i) will always be even, thus we can tell number of healthy branches through casework
-//even - odd = odd > and even - even is even, so we only need to find the parity of infected branches 
+//even - odd = odd and even - even is even, so we only need to find the parity of infected branches 
 
-int bruteForceGrowth(int n); //scroll down to see a brute force implementation
+int bruteForceGrowth(int n); //scroll down to see a brute force implementation this is not acutually needed for the best solution
 //every infected branch infects floor(i^log10(i)+ abs(3cos(i))) for every already infected branch plus an extra i branches (where i is the time)
 
-int growth(int i)
-{
-    return int(floor(pow(i, log10(i))+fabs((3*cos(i)))))%2;
-    //the growth function for branches mod 2
-}
+int growth(int i) //the growth function for branches mod 2 to prevent too alrge numbers from being returned.
+{return int(floor(pow(i, log10(i))+fabs((3*cos(i)))))%2;}
 
 void solve()
 {
     int n; 
     cin>>n;
     int x=1,y=0,z=0; //x is initial, y is the  fractal growth and z is the extra i branches grown at time i
-    //1 represents odd and 0 represents even -> this keeps the memory of the variables small 
-
+    //1 represents odd and 0 represents even -> this keeps the memory of the variables small and eliminates risk of overflow
     for(int i=0; i<=n; i++)
     {
         y=growth(i);
@@ -30,30 +25,23 @@ void solve()
         if(x^(x&y)^z==1){x=1;} //use bitwise operations to determine when the expression is even or odd, another approach is simple casework
         else{x=0;}   
     }
-
     cout<<((x==0)? "EVEN":"ODD")<<endl;
 }
-
 
 int main()
 {
     int t;
     cin>>t;
     while(t--)
-    {
-        solve();
-    }
+    {solve();}
 }
 
-
-
-int bruteForceGrowth(int n)
-{ //this is the brute force calculation of the number of branches at t = n
+int bruteForceGrowth(int n)//this is the brute force calculation of the number of branches at t = n
+{ 
     ll branches=1;
     for(int i=1; i<=n; i++)
     {
-        branches=branches+(branches*(growth(i)))+i;
-        //remove the mod 2 in the growth function to get the actual number of branches
+        branches=branches+(branches*(growth(i)))+i;//remove the mod 2 in the growth function to get the actual number of branches   
     }
     return branches;
 }
