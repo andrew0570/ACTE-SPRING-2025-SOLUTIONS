294fd546de662276003d1228db692236e1ebb442
Vineet Vadrevu <vineetvadrevu@gmail.com>
2025-04-04 00:06:56 -0500

    Add files via upload

---
+++ Problem13.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define dbg(x) cout << #x << " = " << (x) << endl;
+
+
+//notice that the growth is quadratic after modeling it on paper. at n=0, f(n)=4 (starting point) -> so we know the value of c is 4
+//after this, we can do some experimentation to figure out the rest of the quadratic.
+//the formula for the number of bacteria at t=n is: f(n) = n + (n+2)^2 + (n+1)^2 - 1 = 2n^2 + 7x + 4
+
+int f(int n)
+{
+    // return n+((n+2)*(n+2))+((n+1)*(n+1))-1;
+    return (2*n*n) + (7*n) + 4; //this statement and the above statements are equivalent
+}
+
+void solve()
+{
+    int l,r;
+    cin>>l>>r;
+    cout<<f(r)-f(l)<<endl;
+    return;
+}
+
+
+int main()
+{
+    freopen("input1.in","r",stdin);
+    freopen("output1.out","w",stdout);
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+}
